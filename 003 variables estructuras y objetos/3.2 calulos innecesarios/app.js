5efa3eef146b0974ba71d8f465211f8f4409c980
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-21 16:18:23 -0500

    3.2 calculos innecesarios
    usar una variable para no ejecutar la funcion .length en cada iteracion

---
+++ 003 variables estructuras y objetos/3.2 calulos innecesarios/app.js
@@ -0,0 +1,7 @@
+var animales = ["Perro", "Gato", "Leon"] ;
+console.log(animales);
+var total_animales = animales.length; //usar una variable para no ejecutar la funcion .length en cada iteracion
+
+for (var i = 0; i < total_animales; i++) {
+ console.log(animales[i]);
+}
