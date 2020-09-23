b5591fb6356d53673298f1d93a123d7768f00e47
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-23 12:25:21 -0500

    3.13 variables estructuras y objetos, literales

---
+++ 003 variables estructuras y objetos/3.13 literales/app.js
@@ -0,0 +1,11 @@
+var persona = {};
+persona.nombre = "Ponsiano";
+
+var otra_persona = new Object();
+console.log(otra_persona);
+
+var lista = new Array();
+lista[0] = "manzana";
+lista[1] = "pera";
+
+var frutas = ["manzana", "pera"];
