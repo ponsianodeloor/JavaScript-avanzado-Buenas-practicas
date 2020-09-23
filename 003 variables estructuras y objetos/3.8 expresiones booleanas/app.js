d67a8cd996f64f8f32c380703312e740f1c482b8
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-23 10:39:47 -0500

    3.8 variables estructuras y objetos, expresiones booleanas

---
+++ 003 variables estructuras y objetos/3.8 expresiones booleanas/app.js
@@ -0,0 +1,13 @@
+var estado = true, resultado = "";
+
+if (estado) {
+ console.log("estado continuar");
+} else {
+ console.log("estado detenerse");
+}
+
+if (resultado) {
+ console.log("resultado continuar");
+} else {
+ console.log("resultado detenerse");
+}
