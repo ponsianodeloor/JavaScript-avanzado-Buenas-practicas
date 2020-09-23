e51ccc34ff3f5391b9cedec4e8015733ea224330
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-23 10:44:00 -0500

    3.8 variables estructuras y objetos, operador tenario

---
+++ 003 variables estructuras y objetos/3.9 operador ternario/app.js
@@ -0,0 +1,13 @@
+var estado = true, resultado = "";
+
+/*
+if (estado) {
+ console.log("estado continuar");
+} else {
+ console.log("estado detenerse");
+}
+*/
+
+var accion = estado ? 'continuar' : 'detenerse';
+
+console.log(accion);
