992e16e65fb9b30865d6cc224203b1f057e101dd
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-23 10:49:23 -0500

    3.10 variables estructuras y objetos, igualdad
    ===
    //igualdad por tipo y valor sin hacer interpretaciones
    
    ==
    //trata de forzar la conversion de los dos valores para que sea positiva

---
+++ 003 variables estructuras y objetos/3.10 igualdad/app.js
@@ -0,0 +1,4 @@
+//igualdad por tipo y valor sin hacer interpretaciones
+console.log(1 === '1'); //false
+//trata de forzar la conversion de los dos valores para que sea positiva
+console.log(1 == '1'); //true
