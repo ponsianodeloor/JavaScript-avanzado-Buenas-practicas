6c208bb448c32afb33760a208190708add94508f
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-22 20:16:51 -0500

    3.5 variables estructuras y objetos
    Funciones anonimas

--- 003 variables estructuras y objetos/3.2 calulos innecesarios/app.js
+++ 003 variables estructuras y objetos/3.2 calulos innecesarios/app.js
@@ -1,6 +1,7 @@
 var animales = ["Perro", "Gato", "Leon"] ;
 console.log(animales);
-var total_animales = animales.length; //usar una variable para no ejecutar la funcion .length en cada iteracion
+//usar una variable para no ejecutar la funcion .length en cada iteracion
+var total_animales = animales.length;
 
 for (var i = 0; i < total_animales; i++) {
  console.log(animales[i]);
---
+++ 003 variables estructuras y objetos/3.5 funciones anonimas/app.js
@@ -0,0 +1,21 @@
+var tienda = {
+ nombre: "Tienda 6 Skinas",
+ calcular: function(costo1, costo2){
+  let resultado = costo1 + costo2;
+  return resultado;
+ },
+ saludar: function(){
+  var mensaje = "hola";
+  return mensaje;
+ }
+};
+
+console.log(tienda.nombre);
+console.log(tienda.calcular(20, 10));
+console.log(tienda.saludar());
+
+var btnSaludar = document.getElementById("btnSaludar");
+
+btnSaludar.addEventListener('click', function(){
+ console.log(tienda.saludar());
+});
