d596a9a29dd1c45112379070b8428ab1515adc7b
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-24 13:57:17 -0500

    4.8 Estilo en Javascript, JSLint
    sirve para depurar el codigo

--- 004 Estilo en Javascript/4.7 - comillas/app.js
+++ 004 Estilo en Javascript/4.7 - comillas/app.js
@@ -1,5 +1,21 @@
-var mensaje = "La actividad estuvo 'divertida'";
-var mensajeCompleto = 'La actividad estuvo "divertida"';
+var tienda = {
+ nombre: "Tienda 6 Skinas",
+ calcular: function(costo1, costo2) {
+  let resultado = costo1 + costo2;
+  return resultado;
+ },
+ saludar: function() {
+  var mensaje = "hola";
+  return mensaje;
+ }
+};
 
-console.log(mensaje);
-console.log(mensajeCompleto);
+console.log(tienda.nombre);
+console.log(tienda.calcular(20, 10));
+console.log(tienda.saludar());
+
+var btnSaludar = document.getElementById("btnSaludar");
+
+btnSaludar.addEventListener('click', function() {
+ console.log(tienda.saludar());
+});
