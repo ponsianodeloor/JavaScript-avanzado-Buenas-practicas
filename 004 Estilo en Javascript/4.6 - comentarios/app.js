27e39cc8b6a30b2f5f82d71f1fde6a4e39fa63aa
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-24 13:47:51 -0500

    4.5 Estilo en Javascript, comentarios
    para el uso de comentarios instalamos docBlock, este paquete nos permite comentar las funciones creadas de una forma automática

---
+++ 004 Estilo en Javascript/4.6 - comentarios/app.js
@@ -0,0 +1,30 @@
+
+/**
+ * [nombre description]
+ * @type {String}
+ */
+var nombre = "ponsianodeloor",
+    email = "ponsianodeloor@gmail.com";
+
+/**
+ * [saludar description]
+ * @param  {[type]} nombre [description]
+ * @return {[type]}        [description]
+ */
+function saludar(nombre){
+	//Se retorna el mensaje formado
+    return "Hola "+ nombre;
+}
+
+/*
+ Comentario de bloque
+*/
+
+// Pruebas
+// Pruebas
+// Pruebas seleccionar el texto y command + /
+
+function calcular(datoA, datoB, datoC){
+    var resultado = datoA + datoB;
+    return resultado;
+}
