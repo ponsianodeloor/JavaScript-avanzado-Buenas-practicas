373ccf49e09b195759efe658055705ae04416e7d
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-26 17:23:32 -0500

    6.4.- Performance, minificacion y obfuscacion del codigo

---
+++ 006 Performance/6.4 minificar codigo/app.js
@@ -0,0 +1,25 @@
+// var tienda = {
+//  nombre: "Tienda 6 Skinas",
+//  calcular: function(costo1, costo2) {
+//   var resultado = costo1 + costo2;
+//   return resultado;
+//  },
+//  saludar: function() {
+//   var mensaje = "hola";
+//   return mensaje;
+//  }
+// };
+//
+// console.log(tienda.nombre);
+// console.log(tienda.calcular(20, 10));
+// console.log(tienda.saludar());
+//
+// var btnSaludar = document.getElementById("btnSaludar");
+//
+// btnSaludar.addEventListener('click', function() {
+//  console.log(tienda.saludar());
+// });
+
+//var tienda={nombre:"Tienda 6 Skinas",calcular:function(n,a){return n+a},saludar:function(){return"hola"}};console.log(tienda.nombre),console.log(tienda.calcular(20,10)),console.log(tienda.saludar());var btnSaludar=document.getElementById("btnSaludar");btnSaludar.addEventListener("click",function(){console.log(tienda.saludar())});
+
+var _0x8fef=["\x54\x69\x65\x6E\x64\x61\x20\x36\x20\x53\x6B\x69\x6E\x61\x73","\x68\x6F\x6C\x61","\x6E\x6F\x6D\x62\x72\x65","\x6C\x6F\x67","\x63\x61\x6C\x63\x75\x6C\x61\x72","\x73\x61\x6C\x75\x64\x61\x72","\x62\x74\x6E\x53\x61\x6C\x75\x64\x61\x72","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x63\x6C\x69\x63\x6B","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72"];var tienda={nombre:_0x8fef[0],calcular:function(_0xfcfbx2,_0xfcfbx3){var _0xfcfbx4=_0xfcfbx2+ _0xfcfbx3;return _0xfcfbx4},saludar:function(){var _0xfcfbx5=_0x8fef[1];return _0xfcfbx5}};console[_0x8fef[3]](tienda[_0x8fef[2]]);console[_0x8fef[3]](tienda[_0x8fef[4]](20,10));console[_0x8fef[3]](tienda[_0x8fef[5]]());var btnSaludar=document[_0x8fef[7]](_0x8fef[6]);btnSaludar[_0x8fef[9]](_0x8fef[8],function(){console[_0x8fef[3]](tienda[_0x8fef[5]]())})
