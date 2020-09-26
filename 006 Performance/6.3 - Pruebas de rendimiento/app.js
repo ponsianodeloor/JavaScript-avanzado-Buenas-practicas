64f209bbd731975b67445a7a954d6bab1f5a65a8
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-26 17:10:32 -0500

    6.3.- Performance, pruebas de rendimiento

---
+++ 006 Performance/6.3 - Pruebas de rendimiento/app.js
@@ -0,0 +1,10 @@
+console.time('revisionMycode');
+
+var animales = ["perro", "gato", "pez"];
+var cantAnimales = animales.length;
+
+for (var i = 0; i < cantAnimales; i++) {
+ console.log(animales[i]);
+}
+
+console.timeEnd('revisionMycode');
