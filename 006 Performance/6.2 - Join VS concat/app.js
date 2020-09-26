cd7097aa67ccef26d795902814caff0b77de6240
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-26 15:58:37 -0500

    6.2.- Performance, join VS concat

---
+++ 006 Performance/6.2 - Join VS concat/app.js
@@ -0,0 +1,10 @@
+var animales = ["perro", "gato", "pez"];
+
+var listaAnimales = animales.join(",");
+var nuevaListaAnimales = animales.concat("Leon", "Tiburon");
+
+console.log(animales);
+console.log(typeof(listaAnimales));
+console.log(listaAnimales);
+console.log(typeof(nuevaListaAnimales));
+console.log(nuevaListaAnimales);
